303671d3f66bd3221926dcdab8cc38ff4adfc2d8
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-17 11:51:06 +0100

    Changing hello_world.js

--- app/hello_world.js
+++ app/hello_world.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+var server = app.listen(port, function () {
+  console.log("Express server listening on port" + port);
+});
