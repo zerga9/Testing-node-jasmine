5c50a67d9c8b0806fe1b928d78b0f43151e607f9
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-18 10:55:51 +0100

    Rewrite helloWorld specs to use real url

--- spec/helloWorldSpec.js
+++ spec/helloWorldSpec.js
@@ -1,18 +1,16 @@
 var request = require("request");
 
-var base_url = "http://localhost:3000/"
-
 describe("Hello World Server", function(){
   describe("get /", function(){
     it ("returns status code 200",function(){
-      request.get(base_url, function(error, response, body){
+      request.get("http://localhost:3000", function(error, response, body){
         expect(response.statusCode).toBe(200);
         done();//synchronize it with its expect
       });
     });
     it("returns Hello World", function(done) {
-      request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello World");
+      request.get("http://localhost:3000?number=3", function(error, response, body) {
+        expect(body).toBe(JSON.stringify(["Hello World", "Hello World", "Hello World"]));
         done();
       });
     });
