a44090087bf840a9aa4858a0e834085917f850e9
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-18 10:52:09 +0100

    Tests for generator are passing

--- spec/generatorSpec.js
+++ spec/generatorSpec.js
@@ -3,17 +3,17 @@ var generator = require("../app/generator");
 describe("Hello World Generator", function(){
 
   it("returns an array", function(){
-    expect(generator.generateHelloWorlds(0)).toBe([]);
+    expect(generator.generateHelloWorld(0)).toEqual([]);
   });
 
   it("returns the correct number of Hello Worlds", function(){
-    var result = generator.generateHelloWorlds(3);
+    var result = generator.generateHelloWorld(3);
 
     expect(result.length).toBe(3);
   });
 
   it ("returns only Hello Worlds", function(){
-    var result = generator.generateHelloWOrlds(3);
+    var result = generator.generateHelloWorld(3);
 
     result.forEach(function(element){
       expect(element).toBe("Hello World");
