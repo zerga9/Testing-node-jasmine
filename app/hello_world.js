0ddf72d8a1595c0b21db4715edded583b279d775
Gadiza Zerari <gadizaz@hotmail.com>
2018-07-18 11:18:43 +0100

    Rewrite server to use query for amount of Hello Worlds

--- app/hello_world.js
+++ app/hello_world.js
@@ -1,9 +1,14 @@
 var express = require("express");
+var generator = require('./generator');
 var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-  res.send("Hello World");
+  var number = req.query.number;
+  console.log(number);
+  var helloWorldArray = generator.generateHelloWorld(number);
+
+  res.status(200).send(helloWorldArray);
 });
 
 var server = app.listen(port, function () {
